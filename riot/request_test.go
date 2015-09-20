bb512d6467339cb0bcb13e9679d2a5cb8d62bd58
Warren Kenny <warren@ufa.ie>
2015-09-20 20:20:18 +0100

    Formatted request test file

--- riot/request_test.go
+++ riot/request_test.go
@@ -4,25 +4,25 @@ import "testing"
 
 func TestURL(t *testing.T) {
 	cases := []struct {
-		region Region
-		endpoint string
-		version string
-		key string
+		region     Region
+		endpoint   string
+		version    string
+		key        string
 		parameters map[string]string
-		want string
+		want       string
 	}{
-		{ "euw", "champion", "1.2", "1234", make(map[string]string), "https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=1234"},
+		{"euw", "champion", "1.2", "1234", make(map[string]string), "https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=1234"},
 	}
 
 	for _, c := range cases {
-		url, err := URL( c.region, c.endpoint, c.version, c.key, c.parameters )
+		url, err := URL(c.region, c.endpoint, c.version, c.key, c.parameters)
 
 		if err != nil {
-			t.Errorf( "URL(%q, %q, %q, %q, %q) == %q, error %q", c.region, c.endpoint, c.version, c.key, c.parameters, url, err )
+			t.Errorf("URL(%q, %q, %q, %q, %q) == %q, error %q", c.region, c.endpoint, c.version, c.key, c.parameters, url, err)
 		}
 
 		if url != c.want {
-			t.Errorf( "URL(%q, %q, %q, %q, %q) == %q, want %q", c.region, c.endpoint, c.version, c.key, c.parameters, url, c.want )
+			t.Errorf("URL(%q, %q, %q, %q, %q) == %q, want %q", c.region, c.endpoint, c.version, c.key, c.parameters, url, c.want)
 		}
 	}
 }
